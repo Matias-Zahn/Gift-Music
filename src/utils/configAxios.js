f5468586ae0af3b53cdb0f562cda348bff5bc227
Matias Zahn <zahnmatias14@gmail.com>
2024-01-21 15:38:41 -0300

    BaseURL act

--- src/utils/configAxios.js
+++ src/utils/configAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosMusic = axios.create({
-  baseURL: "https://playlist-share.2.us-1.fl0.io",
+  baseURL: "https://music-api27.3.us-1.fl0.io",
 });
 
 axiosMusic.interceptors.request.use((config) => {
